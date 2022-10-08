fdc85b41091875e9d30b78868628e72188d19be4
Matvey <50608884+PM-95025@users.noreply.github.com>
2022-10-08 10:29:38 +0300

    added file edit

--- backend.py
+++ backend.py
@@ -4,7 +4,7 @@ from constants import *
 
 new_password = ''
 for x in range(PASSWORD_LENGTH):
-    new_password += choice(printable)
+	new_password += choice(printable)
 
 # -----------------
 
@@ -13,6 +13,11 @@ message = ftp.login(user=username, passwd=password)
 
 assert message == FTP_SUCCESS_LOGIN
 
-with open('wpa_supplicant.conf', 'wb') as f:
-    ftp.retrbinary(f'RETR {wpa_supplicant}', f.write)
+with open('wpa_supplicant.conf', 'w') as f:
+	ftp.retrlines(f'RETR {wpa_supplicant}', f.write)
 
+with open('wpa_supplicant.conf') as f:
+	for i, line in enumerate(f.readlines()):
+		if 'psk=' not in line:
+			continue
+		line = line[:line.index('psk=') + 4] + password
