93d8e2c54d3e258e118f4e0dd7813eab585b2cb5
Matvey <50608884+PM-95025@users.noreply.github.com>
2022-10-08 10:39:07 +0300

    finished file edit

--- backend.py
+++ backend.py
@@ -17,7 +17,15 @@ with open('wpa_supplicant.conf', 'w') as f:
 	ftp.retrlines(f'RETR {wpa_supplicant}', f.write)
 
 with open('wpa_supplicant.conf') as f:
-	for i, line in enumerate(f.readlines()):
+	lines = f.readlines()
+	for i, line in enumerate(lines):
 		if 'psk=' not in line:
 			continue
 		line = line[:line.index('psk=') + 4] + password
+		line_num = i
+		break
+
+lines[line_num] = line
+
+with open('wpa_supplicant.conf', 'w') as f:
+	f.writelines(lines)
