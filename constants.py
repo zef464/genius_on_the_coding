442a53d25f49c36c3e863b587e071f688d6cb2b8
Matvey <50608884+PM-95025@users.noreply.github.com>
2022-10-09 13:32:03 +0300

    refactor + fix

--- constants.py
+++ constants.py
@@ -1,16 +1,17 @@
 from random import randint
 from string import ascii_letters, digits, punctuation
-from typing import Final
+# from typing import Final
 
-PASSWORD_LENGTH: Final = randint(10, 16)
+PASSWORD_LENGTH = randint(10, 16)
 
 printable = digits + ascii_letters + punctuation
 
-host: Final[str] = '192.168.11.1'
-username: Final[str] = 'pi'
-password: Final[str] = 'butpass1014'
-ftp_port: Final[int] = 21
+host = '192.168.11.1'
+username = 'pi'
+password = 'butpass1014'
+ftp_port = 21
 
-rat_port: Final[int] = 10_000
+rat_port = 10_000
 
-wpa_supplicant: Final[str] = '/etc/wpa_supplicant/wpa_supplicant.conf'
+wpa_supplicant = '/etc/wpa_supplicant/wpa_supplicant.conf'
+# wpa_supplicant = '../wpa_supplicant.conf'
--- rat/server.py
+++ rat/server.py
@@ -1,25 +1,32 @@
+#!/usr/bin/sudo /usr/bin/python3
+
 from random import choice
+# from socket import socket
 from constants import *
 
-new_password = ''
-for x in range(PASSWORD_LENGTH):
-	new_password += choice(printable)
 
-print(new_password)
-# -----------------
+def generate_password():
+	new_password = ''
+	for x in range(PASSWORD_LENGTH):
+		new_password += choice(printable)
 
-with open(wpa_supplicant) as f:
-	lines = f.readlines()
-	for i, line in enumerate(lines):
-		if 'psk=' not in line:
-			continue
-		line = line[:line.index('psk=') + 4] + password
-		line_num = i
-		break
-print('step 1 ready')
+	return new_password
 
-lines[line_num] = line
 
-with open('wpa_supplicant.conf', 'w') as f:
-	f.writelines(lines)
-print('step 2 ready')
+def edit_config(new_password):
+	with open(wpa_supplicant) as f:
+		lines = f.readlines()
+		for i, line in enumerate(lines):
+			if 'psk=' not in line:
+				continue
+			line = line[:line.index('psk=') + 4] + f'"{new_password}"\n'
+			line_num = i
+			break
+
+	lines[line_num] = line
+	with open(wpa_supplicant, 'w') as f:
+		f.writelines(lines)
+
+
+# s = socket()
+# s.connect((host, rat_port))
