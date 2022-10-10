3663eb2e3adafcc8de1d9ca6332d25ce0b3e71b3
Matvey <50608884+PM-95025@users.noreply.github.com>
2022-10-10 09:13:07 +0300

    reindent

--- constants.py
+++ constants.py
@@ -21,36 +21,36 @@ rat_port = 10_000
 MAX_PASSWORD_BYTES = 60
 
 textBrowsers_coords = ((0, 0, 271, 61), # noqa
-		                (0, 60, 31, 31), # noqa
-						(0, 90, 31, 31), # noqa
-						(0, 120, 31, 31), # noqa
-						(0, 170, 271, 41), # noqa
-						(0, 230, 271, 41), # noqa
-						(280, 0, 291, 51), # noqa
-						(290, 60, 271, 31), # noqa
-						(290, 110, 271, 31), # noqa
-						(290, 210, 271, 31), # noqa
-						(290, 160, 271, 31), # noqa
-						(290, 260, 271, 31),) # noqa
-textBrowsers_values = ("Mac-адреса:", # noqa
-		                       "1)", # noqa
-		                       "2)", # noqa
-		                       "3)", # noqa
-		                       "Активный пароль:", # noqa
-		                       "qwerty", # noqa
-		                       "Данные:", # noqa
-		                       "Привет)", # noqa
-		                       "Как дела???", # noqa
-		                       "^_^", # noqa
-		                       "Координаты: 34, 56", # noqa
-		                       "Пока!",) # noqa
+		               (0, 60, 31, 31), # noqa
+					   (0, 90, 31, 31), # noqa
+					   (0, 120, 31, 31), # noqa
+					   (0, 170, 271, 41), # noqa
+					   (0, 230, 271, 41), # noqa
+					   (280, 0, 291, 51), # noqa
+					   (290, 60, 271, 31), # noqa
+					   (290, 110, 271, 31), # noqa
+					   (290, 210, 271, 31), # noqa
+					   (290, 160, 271, 31), # noqa
+					   (290, 260, 271, 31),) # noqa
+textBrowsers_values = ("Mac-адреса:",
+		               "1)", # noqa
+		               "2)", # noqa
+		               "3)", # noqa
+		               "Активный пароль:", # noqa
+		               "qwerty", # noqa
+		               "Данные:", # noqa
+		               "Привет)", # noqa
+		               "Как дела???", # noqa
+		               "^_^", # noqa
+		               "Координаты: 34, 56", # noqa
+		               "Пока!",) # noqa
 lineEdit_coords = ((70, 60, 201, 21), # noqa
-		                   (70, 120, 201, 22), # noqa
-		                   (70, 90, 201, 22), # noqa
-		                   (40, 60, 16, 20),) # noqa
+		           (70, 120, 201, 22), # noqa
+		           (70, 90, 201, 22), # noqa
+		           (40, 60, 16, 20),) # noqa
 checkBox_coords = ((40, 60, 16, 20), # noqa
-						   (40, 90, 16, 20), # noqa
-						   (40, 130, 16, 20),) # noqa
+				   (40, 90, 16, 20), # noqa
+				   (40, 130, 16, 20),) # noqa
 
 htmls = (
 	"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\np, li { white-space: pre-wrap; }\n</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt; font-weight:600;\">Mac-адреса:</span></p></body></html>", # noqa
