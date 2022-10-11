c5d93c726dd1342cca7a70af30689fcc3c2b39ac
Matvey <50608884+PM-95025@users.noreply.github.com>
2022-10-11 09:06:29 +0300

    very major fix

--- constants.py
+++ constants.py
@@ -4,14 +4,15 @@ from platform import platform
 
 PASSWORD_LENGTH = randint(10, 16)
 
+punctuation = punctuation.replace('"', '').replace("'", '').replace('\\', '')
 printable = digits + ascii_letters + punctuation
 
 if 'Windows' in platform():
 	wpa_supplicant = '../wpa_supplicant.conf'
-	host = 'localhost'
+	host = '192.168.11.103'
 else:
 	wpa_supplicant = '/etc/wpa_supplicant/wpa_supplicant.conf'
-	host = '192.168.11.1'
+	host = '192.168.11.103'
 
 ftp_username = 'pi'
 ftp_password = 'butpass1014' # noqa
@@ -20,10 +21,10 @@ ftp_port = 21
 rat_port = 10_000
 MAX_PASSWORD_BYTES = 60
 
-textBrowsers_coords = ((0, 0, 271, 61), # noqa
-		               (0, 60, 31, 31), # noqa
-					   (0, 90, 31, 31), # noqa
-					   (0, 120, 31, 31), # noqa
+textBrowsers_coords = (# (0, 0, 271, 61), # noqa
+		               # (0, 60, 31, 31), # noqa
+					   # (0, 90, 31, 31), # noqa
+					   # (0, 120, 31, 31), # noqa
 					   (0, 170, 271, 41), # noqa
 					   (0, 230, 271, 41), # noqa
 					   (280, 0, 291, 51), # noqa
@@ -32,10 +33,10 @@ textBrowsers_coords = ((0, 0, 271, 61), # noqa
 					   (290, 210, 271, 31), # noqa
 					   (290, 160, 271, 31), # noqa
 					   (290, 260, 271, 31),) # noqa
-textBrowsers_values = ("Mac-адреса:",
-		               "1)", # noqa
-		               "2)", # noqa
-		               "3)", # noqa
+textBrowsers_values = (# "Mac-адреса:", # noqa
+		               # "1)", # noqa
+		               # "2)", # noqa
+		               # "3)", # noqa
 		               "Активный пароль:", # noqa
 		               "", # noqa
 		               "Данные:", # noqa
@@ -44,6 +45,7 @@ textBrowsers_values = ("Mac-адреса:",
 		               "^_^", # noqa
 		               "Координаты: 34, 56", # noqa
 		               "Пока!",) # noqa
+"""
 lineEdit_coords = ((70, 60, 201, 21), # noqa
 		           (70, 120, 201, 22), # noqa
 		           (70, 90, 201, 22), # noqa
@@ -51,12 +53,13 @@ lineEdit_coords = ((70, 60, 201, 21), # noqa
 checkBox_coords = ((40, 60, 16, 20), # noqa
 				   (40, 90, 16, 20), # noqa
 				   (40, 130, 16, 20),) # noqa
+"""
 
 htmls = (
-	"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\np, li { white-space: pre-wrap; }\n</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt; font-weight:600;\">%s</span></p></body></html>", # noqa
-	"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\np, li { white-space: pre-wrap; }\n</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:7pt; text-decoration: underline;\">%s</span></p></body></html>", # noqa
-	"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\np, li { white-space: pre-wrap; }\n</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">%s</p></body></html>", # noqa
-	"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\np, li { white-space: pre-wrap; }\n</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">%s</p></body></html>", # noqa
+	# "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\np, li { white-space: pre-wrap; }\n</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt; font-weight:600;\">%s</span></p></body></html>", # noqa
+	# "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\np, li { white-space: pre-wrap; }\n</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:7pt; text-decoration: underline;\">%s</span></p></body></html>", # noqa
+	# "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\np, li { white-space: pre-wrap; }\n</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">%s</p></body></html>", # noqa
+	# "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\np, li { white-space: pre-wrap; }\n</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">%s</p></body></html>", # noqa
 	"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\np, li { white-space: pre-wrap; }\n</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt; font-weight:600;\">%s</span></p></body></html>", # noqa
 	"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\np, li { white-space: pre-wrap; }\n</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">%s</span></p></body></html>", # noqa
 	"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\np, li { white-space: pre-wrap; }\n</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt; font-weight:600;\">%s</span></p></body></html>", # noqa
